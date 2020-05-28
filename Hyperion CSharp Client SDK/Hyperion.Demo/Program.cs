97c0794d830a275c4b9dc8f626e8c962704ab0d4
Justin Stay <jlstay@micronoptics.com>
2020-05-28 12:59:17 -0400

    Updated example program to exercise the FP sensors and sensor stream.

--- Hyperion CSharp Client SDK/Hyperion.Demo/Program.cs
+++ Hyperion CSharp Client SDK/Hyperion.Demo/Program.cs
@@ -1,20 +1,17 @@
-﻿using MicronOptics.Hyperion.Communication;
+﻿using System.Net.Sockets;
+using static System.Console;
+using MicronOptics.Hyperion.Communication;
 using MicronOptics.Hyperion.Communication.Sensors;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MicronOptics.Hyperion.Demo
+namespace MicronOptics.Hyperion.Console
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			// *** CHANGE TO THE IP ADDRESS FOR INSTRUMENT ***
-			string instrumentIpAddress = args.Length > 0 ? args[0] : "10.0.0.55";
+			string instrumentIpAddress = "192.168.86.70";
 
 			// Create a TCP client for communicating with the Hyperion instrument
 			TcpClient tcpClient = new TcpClient();
@@ -33,13 +30,13 @@ namespace MicronOptics.Hyperion.Demo
 			// The response (unless specifically suppressed) contains an ASCII based Message field
 			// AND a binary (byte[]) Content field. The ASCII field is intended to be human readable
 			// and the binary data is intended to be easily parsed by a computer.
-			Console.WriteLine("Instrument Serial Number");
-			Console.WriteLine("------------------------");
-			Console.WriteLine($"Message: {response.Message}");
-			Console.WriteLine($"Content Converted to String {response.AsString()}");
+			WriteLine("Instrument Serial Number");
+			WriteLine("------------------------");
+			WriteLine($"Message: {response.Message}");
+			WriteLine($"Content Converted to String {response.AsString()}");
 
-			Console.WriteLine();
-			Console.WriteLine();
+			WriteLine();
+			WriteLine();
 
 			// Execute the #GetPeaks command. The CommandName contains static string members
 			// for commands exposed by the instrument.
@@ -52,16 +49,16 @@ namespace MicronOptics.Hyperion.Demo
 			// int, double, and more complex responses such as Peak/Spectrum data
 			PeakData peakData = response.AsPeakData();
 
-			Console.WriteLine("Peak Data Header");
-			Console.WriteLine("---------------------");
+			WriteLine("Peak Data Header");
+			WriteLine("---------------------");
 
-			Console.WriteLine($"Serial Number: {peakData.SerialNumber}");
-			Console.WriteLine($"Timestamp: {peakData.Timestamp}");
+			WriteLine($"Serial Number: {peakData.SerialNumber}");
+			WriteLine($"Timestamp: {peakData.Timestamp}");
 
-			Console.WriteLine();
+			WriteLine();
 
-			Console.WriteLine("Peaks");
-			Console.WriteLine("-----");
+			WriteLine("Peaks");
+			WriteLine("-----");
 
 			// The PeakData exposes the peaks in two ways...as Arrays and as Enumerables. The
 			// arrays will allocate new memory for the data but can be advantageous if the 
@@ -71,16 +68,16 @@ namespace MicronOptics.Hyperion.Demo
 			// peaks at very high speeds.
 			for (int channelIndex = 0; channelIndex < 4; channelIndex++)
 			{
-				Console.WriteLine($"Channel {channelIndex}: ");
+				WriteLine($"Channel {channelIndex}: ");
 
 				foreach (double wavelength in peakData.AsEnumerable(1))
 				{
-					Console.WriteLine($"\t{wavelength} nm");
+					WriteLine($"\t{wavelength} nm");
 				}
 			}
 
-			Console.WriteLine();
-			Console.WriteLine();
+			WriteLine();
+			WriteLine();
 
 
 
@@ -90,24 +87,58 @@ namespace MicronOptics.Hyperion.Demo
 			// Use the extension methods to easily obtain the spectrum data
 			SpectrumData spectrumData = response.AsSpectrumData();
 
-			Console.WriteLine("Full Spectrum");
-			Console.WriteLine("-------------");
-			Console.WriteLine($"Wavelength Start: {spectrumData.WavelengthStart:F3} nm");
-			Console.WriteLine($"Wavelength Step: {(int)(1000 * spectrumData.WavelengthStep)} pm");
-			Console.WriteLine($"Wavelength Step Count: {spectrumData.WavelengthStepCount}");
-			Console.WriteLine($"Channel Count: {spectrumData.ChannelCount}");
-			Console.WriteLine($"Serial Number: {spectrumData.SerialNumber}");
-			Console.WriteLine($"Timestamp: {spectrumData.Timestamp}");
+			WriteLine("Full Spectrum");
+			WriteLine("-------------");
+			WriteLine($"Wavelength Start: {spectrumData.WavelengthStart:F3} nm");
+			WriteLine($"Wavelength Step: {(int)(1000 * spectrumData.WavelengthStep)} pm");
+			WriteLine($"Wavelength Step Count: {spectrumData.WavelengthStepCount}");
+			WriteLine($"Channel Count: {spectrumData.ChannelCount}");
+			WriteLine($"Serial Number: {spectrumData.SerialNumber}");
+			WriteLine($"Timestamp: {spectrumData.Timestamp}");
 
-			Console.WriteLine();
-			Console.WriteLine();
+			WriteLine();
+			WriteLine();
 
+			// Remove existing sensors
+			response = Command.Execute(tcpNetworkStream, CommandOptions.None, CommandName.GetSensorNames);
+			string[] sensorNames = response.AsString().Split(' ');
+			foreach (string sensorName in sensorNames)
+			{
+				WriteLine($"Removing sensor: {sensorName}");
+				response = Command.Execute(tcpNetworkStream, CommandOptions.None, CommandName.RemoveSensor, sensorName);
+			}
 
+			// Add some sensors
+			for (int i = 1; i <= 20; i++)
+			{
+				string name = $"sensor_{i}";
+				string model = i % 2 == 1 ? "os7510" : "os7520";
+				int channel = (i - 1) % 4 + 1;
+				int wavelength = 1510 + ((i - 1) % 4) * 20;
+				bool fixedOrientation = i % 2 == 0;
+				double calibration = 10.0 * i;
+				WriteLine($"Adding Sensor {name}");
+				WriteLine("----------------------------------------");
+				WriteLine($"Model: {model}");
+				WriteLine($"Channel: {channel}");
+				WriteLine($"Wavelength Band: {wavelength} nm");
+				WriteLine($"Calibration Factor: {calibration} nm/g");
+				WriteLine($"Fixed Orientation: {fixedOrientation}");
+				WriteLine();
+				string[] input_args = {name,
+					model,
+					channel.ToString(),
+					"0",
+					wavelength.ToString(),
+					calibration.ToString(),
+					fixedOrientation.ToString()};
+				response = Command.Execute(tcpNetworkStream, CommandOptions.None, CommandName.AddSensor, input_args);
+			}
 
 			// Retrieve the defined Sensors
 			response = Command.Execute(tcpNetworkStream, CommandOptions.None, CommandName.ExportSensors);
 
-			Console.WriteLine(response.Message);
+			WriteLine(response.Message);
 
 			int offset = 0;
 
@@ -118,29 +149,27 @@ namespace MicronOptics.Hyperion.Demo
 			offset += sizeof(UInt16);
 
 			// Sensor Count
-			Console.WriteLine($"Sensors - {numberOfSensors} (Data Export Version = {version})");
-			Console.WriteLine();
+			WriteLine($"Sensors - {numberOfSensors} (Data Export Version = {version})");
+			WriteLine();
 
 			// Create sensors
 
 			for (int sensorIndex = 0; sensorIndex < numberOfSensors; sensorIndex++)
 			{
 				FabryPerotAccelerometer fpSensor = (FabryPerotAccelerometer)SensorBase.Create(response.Content, ref offset);
-
-				Console.WriteLine($"Sensor {sensorIndex + 1} - {fpSensor.Name} ({fpSensor.Model})");
-				Console.WriteLine("----------------------------------------");
-				Console.WriteLine($"ID: {fpSensor.Id}");
-				Console.WriteLine($"Model: {fpSensor.Model}");
-				Console.WriteLine($"Channel: {fpSensor.DutChannelIndex + 1}");
-				Console.WriteLine($"Wavelength Band: {fpSensor.WavelengthBand} nm");
-				Console.WriteLine($"Calibration Factor: {fpSensor.CalibrationFactor} m");
-				Console.WriteLine($"Delta-N Over Range Threshold: {fpSensor.DnFromPeakOverRangeThreshold}");
-				Console.WriteLine($"Delta-N Peak Threshold: {fpSensor.DnFromPeakThreshold}");
-				Console.WriteLine($"Recenter Threshold Low: {fpSensor.RecenterThresholdLow} nm^2");
-				Console.WriteLine();
+				WriteLine($"Sensor {sensorIndex + 1} - {fpSensor.Name} ({fpSensor.Model})");
+				WriteLine($"Sensor Definition Version: {fpSensor.FPSesnorVersion}");
+				WriteLine("----------------------------------------");
+				WriteLine($"ID: {fpSensor.Id}");
+				WriteLine($"Model: {fpSensor.Model}");
+				WriteLine($"Channel: {fpSensor.DutChannelIndex + 1}");
+				WriteLine($"Wavelength Band: {fpSensor.WavelengthBand} nm");
+				WriteLine($"Calibration Factor: {fpSensor.CalibrationFactor} nm/g");
+				WriteLine($"Fixed Orientation: {fpSensor.FixedOrientation}");
+				WriteLine();
 			}
 
-			Console.WriteLine();
+			WriteLine();
 
 
 
@@ -164,25 +193,59 @@ namespace MicronOptics.Hyperion.Demo
 			// defined when the reader is created.
 			StreamingDataReader reader = new StreamingDataReader(StreamingDataMode.Peaks);
 
-			Console.WriteLine("Streaming Peak Data Acquistion Serial Numbers");
-			Console.WriteLine("---------------------------------------------");
+			WriteLine("Streaming Peak Data Acquistion Serial Numbers");
+			WriteLine("---------------------------------------------");
 
 			for (int index = 0; index < 10; index++)
 			{
-				Console.WriteLine(
+				WriteLine(
 					$"{index}: " +
 					$"{ reader.ReadStreamingData(tcpNetworkStream).AsPeakData().SerialNumber}");
 			}
 
-			Console.WriteLine();
+			WriteLine();
 
 			tcpNetworkStream.Close();
 			tcpClient.Close();
 
 
+
+			// Now demonstrating using the data streaming to continuously read consecutive
+			// sensor data sets.
+			tcpClient = new TcpClient();
+			tcpClient.Connect(instrumentIpAddress, StreamingDataReader.SensorTcpPort);
+			tcpNetworkStream = tcpClient.GetStream();
+
+			// The StreamingDataReader class works for peaks, full spectrum and sensors. It can be
+			// used to easily and efficiently read consecutive datasets. The class internally
+			// uses a single buffer of memory to avoid allocating and collecting large
+			// amounts of memory and system resources. The mode Peak, Spectrum, Sensor) is
+			// defined when the reader is created.
+			StreamingDataReader sensorReader = new StreamingDataReader(StreamingDataMode.Sensor);
+
+			WriteLine("Streaming Sensor Data Acquistion Serial Numbers");
+			WriteLine("---------------------------------------------");
+
+			for (int index = 0; index < 10; index++)
+			{
+				SensorData sensorData = sensorReader.ReadStreamingData(tcpNetworkStream).AsSensorData();
+
+				WriteLine(
+					$"{index}: " +
+					$"{sensorData.SerialNumber}");
+
+			}
+
+			WriteLine();
+
+			tcpNetworkStream.Close();
+			tcpClient.Close();
+
+
+
+
 			// Wait for any key press to exit
-			Console.ReadLine();
+			ReadLine();
 		}
 	}
 }
-
